8469dd9dbce0ef682446ac8fa9db8a8436109beb
Luis Sanchez <luisanchezdh@gmail.com>
2023-01-11 12:03:36 -0500

    init webpack and html template

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Travel App',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+}
