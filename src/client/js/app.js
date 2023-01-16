0744a236d3bda1427d205540774b393287deb453
Luis Sanchez <luisanchezdh@gmail.com>
2023-01-16 13:41:26 -0500

    console.log hello

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,14 +1,1 @@
-let trip = {};
-
-const getCountry = async (geonamesKEY) => {
-  const response = await fetch(`http://api.geonames.org/searchJSON?username=luisdaniel&q=${trip.city}&maxRows=1`);
-  try {
-    const data = await response.json();
-    console.log(data)
-    trip.country = data.geonames[0].countryName;
-  } catch (error) {
-    trip.country = "ooops, country does not exist";
-    console.log("error");
-  }
-}
-getCountry();
+console.log("hello");
