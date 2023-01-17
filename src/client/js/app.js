dd4aca6bc5b0c15f977740fea2f50085c65995aa
Luis Sanchez <luisanchezdh@gmail.com>
2023-01-17 12:32:52 -0500

    added innerHTML

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -4,19 +4,23 @@ const button = document.querySelector("#button")
 
 const apiUsername = 'luisdaniel';
 const placeName = document.querySelector("#locationForm")
+const country = document.querySelector("#city")
 
 const fetchGeo = async () => {
   try {
     const response = await axios.get(`http://api.geonames.org/searchJSON?name=${placeName.value}&username=${apiUsername}`)
       .then(apidata => {
-        console.log(apidata);
+        // console.log(apidata);
         const latitude = apidata.data.geonames[0].lat;
         const longitude = apidata.data.geonames[0].lng;
         const countryName = apidata.data.geonames[0].countryName;
+        country.innerHTML = `Country: ${countryName}`
         console.log(latitude);
         console.log(longitude);
-        console.log(countryName)
+        console.log(countryName);
+        return response
       })
+
 
 
   } catch (error) {
