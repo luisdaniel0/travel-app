63e3356ad48006c4c5b24446244daca7ffcc6f7b
Luis Sanchez <luisanchezdh@gmail.com>
2023-01-16 19:13:13 -0500

    fetch geo api, showing lat, long and country name

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,1 +1,31 @@
-console.log("hello");
+import axios from 'axios';
+
+const button = document.querySelector("#button")
+
+const apiUsername = 'luisdaniel';
+const placeName = document.querySelector("#locationForm")
+
+const fetchGeo = async () => {
+  try {
+    const response = await axios.get(`http://api.geonames.org/searchJSON?name=${placeName.value}&username=${apiUsername}`)
+      .then(apidata => {
+        console.log(apidata);
+        const latitude = apidata.data.geonames[0].lat;
+        const longitude = apidata.data.geonames[0].lng;
+        const countryName = apidata.data.geonames[0].countryName;
+        console.log(latitude);
+        console.log(longitude);
+        console.log(countryName)
+      })
+
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetchGeo();
+})
+
--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 let projectData = {};
 
+
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(
@@ -17,3 +19,14 @@ app.use(express.static('dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+// Server Setup and Routes
+const port = 8081;
+app.listen(port, function () {
+  console.log(`Congratulations, your server is running at port ${port}!`);
+});
+
+app.get('/', function (req, res) {
+  res.sendFile('dist/index.html');
+});
+
